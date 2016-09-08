0f39bfc3c903d7ea66b05422f1aa8be413aaa10b
yuxiangque <yuxiangque@outlook.com>
2016-09-08 19:54:04 +0800

    - Font Size

--- src/main/java/com/placeholder/test/mstest2016april1/FontSize.java
+++ src/main/java/com/placeholder/test/mstest2016april1/FontSize.java
@@ -2,50 +2,38 @@ package com.placeholder.test.mstest2016april1;
 
 import java.util.Scanner;
 
-/**
- * @author yuxiangque
- * @version 2016/4/18
- */
 public class FontSize {
-
-    public static int fontSize(int P, int W, int H, int[] a) {
-        int emptyLines = Integer.MAX_VALUE;  // 剩余空行数目
-        int fontSize = -1;
-        int maxS = Math.min(H, W);  // 1 character at least
-        for (int S = maxS; S >= 1; --S) {
-            int maxNumOfLines = P * (H / S);
-            int numOfLines = 0;
-            for (int i = 0; i < a.length; i++) {
-                numOfLines += Math.ceil(a[i] / (W / S));  //  show ⌊W / S⌋ characters in a line
-                if (numOfLines > maxNumOfLines) {
-                    break;
-                }
-            }
-            if (numOfLines > maxNumOfLines) {
-                continue;
-            }
-            int empty = maxNumOfLines - numOfLines;
-            if (empty < emptyLines) {
-                emptyLines = empty;
-                fontSize = S;
-            }
-        }
-        return fontSize;
-    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int TASKS = scanner.nextInt();
-        for (int i = 0; i < TASKS; i++) {
-            int N = scanner.nextInt();
-            int P = scanner.nextInt();
-            int W = scanner.nextInt();
-            int H = scanner.nextInt();
-            int[] a = new int[N];
-            for (int j = 0; j < N; j++) {
+        int tasks = scanner.nextInt();
+        for (int i = 0; i < tasks; i++) {
+            int n = scanner.nextInt();
+            int p = scanner.nextInt();
+            int w = scanner.nextInt();
+            int h = scanner.nextInt();
+            int[] a = new int[n];
+            for (int j = 0; j < n; j++) {
                 a[j] = scanner.nextInt();
             }
-            System.out.println(fontSize(P, W, H, a));
+            System.out.println(fontSize(n, p, w, h, a));
         }
+    }
+
+    private static int fontSize(int n, int p, int w, int h, int[] a) {
+        // w/s h/s
+        int maxS = Math.min(w, h);
+        for (int s = maxS; s >= 1; --s) {
+            int maxLines = p * (h / s);
+            int maxChars = w / s;
+            for (int i = 0; i < n; i++) {
+                int lines = a[i] / maxChars + (a[i] % maxChars == 0 ? 0 : 1);
+                maxLines -= lines;
+            }
+            if (maxLines >= 0) {
+                return s;
+            }
+        }
+        return 0;
     }
 }
